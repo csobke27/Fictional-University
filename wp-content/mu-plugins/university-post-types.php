fc5dcf8b62cd53f564535922807284a4b51c2f6b
csobke27 <csobke@gmail.com>
2025-04-03 19:47:37 -0400

    updated front-page events and posts to use excerpt if available

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,8 @@
     function fictional_university_post_types() {
         // Register a custom post type for programs
         register_post_type('event', array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor', 'excerpt'),
             'rewrite' => array('slug' => 'events'),
             'has_archive' => true,
             'public' => true,
