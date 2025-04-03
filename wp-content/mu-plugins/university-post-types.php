1e6a06301d9cd25bf82dd0365eab2968eb2affcf
csobke27 <csobke@gmail.com>
2025-04-03 19:06:57 -0400

    added custom post type 'event', made event plugin required, created archive for events

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,20 @@
+<?php
+    function fictional_university_post_types() {
+        // Register a custom post type for programs
+        register_post_type('event', array(
+            'rewrite' => array('slug' => 'events'),
+            'has_archive' => true,
+            'public' => true,
+            'labels' => array(
+                'name' => 'Events',
+                'add_new_item' => 'Add New Event',
+                'edit_item' => 'Edit Event',
+                'all_items' => 'All Events',
+                'singular_name' => 'Event',
+            ),
+            'menu_icon' => 'dashicons-calendar'
+        ));
+    }
+
+    add_action('init', 'fictional_university_post_types');
+?>
