3b5bd1095413b1194f746c8e76255da9701c921a
csobke27 <csobke@gmail.com>
2025-04-06 16:47:32 -0400

    adding program post type and pages

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,6 @@
 <?php
     function fictional_university_post_types() {
-        // Register a custom post type for programs
+        // Register a custom post type for events
         register_post_type('event', array(
             'show_in_rest' => true,
             'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +15,23 @@
                 'singular_name' => 'Event',
             ),
             'menu_icon' => 'dashicons-calendar'
+        ));
+
+        // Register a custom post type for programs
+        register_post_type('program', array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor'),
+            'rewrite' => array('slug' => 'programs'),
+            'has_archive' => true,
+            'public' => true,
+            'labels' => array(
+                'name' => 'Programs',
+                'add_new_item' => 'Add New Program',
+                'edit_item' => 'Edit Program',
+                'all_items' => 'All Programs',
+                'singular_name' => 'Program',
+            ),
+            'menu_icon' => 'dashicons-book-alt'
         ));
     }
 
--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -24,6 +24,11 @@
     // Add custom post query for events and if the query is the main query
     // and if the query is not in the admin area
     function fictional_university_adjust_queries($query) {
+        if(!is_admin() && is_post_type_archive('program') && $query->is_main_query()) {
+            $query->set('orderby', 'title');
+            $query->set('order', 'ASC');
+            $query->set('posts_per_page', -1);
+        }
         if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
             $today = date('Ymd');
             $query->set('meta_key', 'event_date');
